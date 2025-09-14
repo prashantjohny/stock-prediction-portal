ae878c5af1b98d492c9a7424a1c26e39a2976ae8
prashantjohny <prasp3677@gmail.com>
2025-09-14 20:55:33 +0530

    Creating Homepage through react

---
+++ frontend-react/src/components/Main.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Button from './Button'
+
+const Main = () => {
+  return (
+    <>
+        <div className='container'>
+            <div className='p-5 text-center bg-light-dark rounded'>
+                <h1 className='text-light'>Stock Vision</h1>
+                <p className='text-light lead'>This Stock Prediction application utilizes machine learning techniques, specifically employing Keras, and LSTM model, integrated with in the Django framework. It forecasts future stock prices by analyzing 100-days and 200-days moving averages, essential indicators widely used by stock analysts to inform trading and investment decisions.</p>
+                <Button text="Login" class="btn-outline-info"/>
+
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Main
