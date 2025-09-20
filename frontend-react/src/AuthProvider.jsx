7de953d97fda49682e1605ce3535ca350d915e85
prashantjohny <prasp3677@gmail.com>
2025-09-20 14:22:14 +0530

    Login and Logout functionality by jwt

---
+++ frontend-react/src/AuthProvider.jsx
@@ -0,0 +1,20 @@
+import React,{useState,useContext} from 'react'
+import { createContext } from 'react';
+
+//Create the context
+const AuthContext = createContext();
+
+const AuthProvider = ({children}) => {
+
+    const [isLoggedIn, setIsLoggedIn]=useState(
+        !!localStorage.getItem('accessToken')
+    )
+  return (
+    <AuthContext.Provider value={{isLoggedIn, setIsLoggedIn}}>
+        {children}
+    </AuthContext.Provider>
+  )
+}
+
+export default AuthProvider
+export {AuthContext}
