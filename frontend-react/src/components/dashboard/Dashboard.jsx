8408eb3a3e332d71d38dc00d46546a77840fab1f
prashantjohny <prasp3677@gmail.com>
2025-12-09 20:46:58 +0530

    integrating machine learning model with django and react application

--- frontend-react/src/components/dashboard/Dashboard.jsx
+++ frontend-react/src/components/dashboard/Dashboard.jsx
@@ -1,14 +1,28 @@
-import React,{useEffect} from 'react'
+import React,{useEffect, useState} from 'react'
 // import axios from 'axios'
 import axiosInstance from '../../axiosinstance'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSpinner } from '@fortawesome/free-solid-svg-icons'
 
 const Dashboard = () => {
-    
+    const [ticker, setTicker] = useState()
+    const [error,setError]=useState()
+    const [loading, setLoading]=useState(false)
+    const [plot, setPlot] = useState()
+    const [ma100, setMA100] = useState()
+    const [ma200, setMA200] = useState()
+    const [prediction, setPrediction]=useState()
+    const [zoom, setZoom] = useState()
+    const [mse, setMSE] = useState()
+    const [rmse, setRMSE] = useState()
+    const [r2, setR2] = useState()
+
+
     useEffect(()=>{
         const fetchProtectedData= async ()=>{
             try{
                 const response= await axiosInstance.get('/protected-view/');
-                console.log('Success:',response.data);
+                
 
             }
             catch(error){
@@ -17,8 +31,97 @@ const Dashboard = () => {
         }
         fetchProtectedData();
     }, [])
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setLoading(true)
+        try{
+            const response = await axiosInstance.post('/predict/', {
+                ticker: ticker
+            });
+            console.log(response.data);
+            const backendRoot=import.meta.env.VITE_BACKEND_ROOT
+            const ploturl = `${backendRoot}${response.data.plot_img}`
+            const ma100Url = `${backendRoot}${response.data.plot_100_dma}`
+            const ma200Url = `${backendRoot}${response.data.plot_200_dma}`
+            const predictionUrl = `${backendRoot}${response.data.plot_prediction}`
+            const zoomUrl = `${backendRoot}${response.data.zoom_prediction}`
+
+            setPlot(ploturl)
+            setMA100(ma100Url)
+            setMA200(ma200Url)
+            setPrediction(predictionUrl)
+            setMSE(response.data.mse)
+            setRMSE(response.data.rmse)
+            setR2(response.data.r2)
+            setZoom(zoomUrl)
+            //Set Plot
+            if(response.data.error){
+                setError(response.data.error)
+            }
+        }
+        catch(error){
+            console.error("There was an error making the API request",error)
+        }
+        finally{
+            setLoading(false);
+        }
+    }
+
   return (
-    <div className='text-light container'>Dashboard</div>
+    <div className='container'>
+        <div class='row'>
+            <div className="col-md-6 mx-auto">
+                <form onSubmit={handleSubmit}>
+                    <input type='text' className='form-control' placeholder='Enter Stock Ticker'
+                    onChange={(e) => setTicker(e.target.value)} required/>
+                    <small>{error && <div className='text-danger'>{error}</div>}</small>
+                    <button type='submit' className='btn btn-info mt-3'>
+                        { loading ? <span><FontAwesomeIcon icon={faSpinner} spin/> Wait & Acknowledge to PTG...</span> : "See Prediction"}
+                    </button>
+                </form>
+            </div>
+            {/* Print prediction plots*/}
+            {prediction && (
+                <div className="prdiction mt-5">
+                <div className="p-3">
+                    {plot && (
+                        <img src={plot} style={{ maxwidth: '100%'}}/>
+                    )}
+
+                </div>
+                <div className="p-3">
+                    {ma100 && (
+                        <img src={ma100} style={{ maxwidth: '100%'}}/>
+                    )}
+                </div>
+                <div className="p-3">
+                    {ma200 && (
+                        <img src={ma200} style={{ maxwidth: '100%'}}/>
+                    )}
+                </div>
+                <div className="p-3">
+                    {prediction && (
+                        <img src={prediction} style={{ maxwidth: '100%'}}/>
+                    )}
+                </div>
+                <div className="p-3">
+                    {zoom && (
+                        <img src={zoom} style={{ maxwidth: '100%'}}/>
+                    )}
+                </div>
+                <div className="text-light p-3">
+                    <h4>Model Evaluation</h4>
+                    <p>Mean Squared Error (MSE): {mse}</p>
+                    <p>Root Mean Squared Error (MSE): {rmse}</p>
+                    <p>R-Squared (r2): {r2}</p>
+
+                </div>
+            </div>
+            )}
+            
+        </div>
+    </div>
   )
 }
 
